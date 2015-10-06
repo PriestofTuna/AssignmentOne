1812e5031e5e02aa45a86d6339e54fd2adf8ffdd
AsianBosses <phoboi1@gmail.com>
2015-10-06 18:27:06 -0400

    Update Assign1.java

--- Assignment1_hw/src/Assign1.java
+++ Assignment1_hw/src/Assign1.java
@@ -1,23 +1,55 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class Assign1 {
+public class Assignment3 {
+
+	public enum AccountType {
+		Admin, Staff, Student
+	};
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String color, food, animal, name;
+		String user = "cqd2292";
+		String pass = "password";
+		// String user2 = "cdinh";
+		// String pass2 = "password1";
+		// String user3 = "Tanes";
+		// String pass3 = "password2";
+		AccountType[] account = { AccountType.Admin, AccountType.Staff, AccountType.Student };
+		String euser, epass;
+		int limit = 3;
 
-		System.out.println("What is your favorite color? ");
-		color = scan.nextLine();
-		System.out.println("What is your favorite food? ");
-		food = scan.nextLine();
-		System.out.println("What is your favorite animal? ");
-		animal = scan.nextLine();
-		System.out.println("What is your name? ");
-		name = scan.nextLine();
+		euser = JOptionPane.showInputDialog("Enter Username");
 
-		System.out
-				.println("I had a dream that " + name + " ate a " + color + " "
-						+ animal + "\n" + "and said it tasted like " + food + "!");
+		while (euser != user) {
+
+			if (euser.equals(user)) {
+				epass = JOptionPane.showInputDialog("Enter Password");
+				if (epass.equals(pass)) {
+					// JOptionPane.showMessageDialog(null, "Hello");
+					AccountType select = (AccountType) JOptionPane.showInputDialog(null, "Select account type",
+							"Account Type", JOptionPane.INFORMATION_MESSAGE, null, account, account[0]);
+					switch (select) {
+					case Admin:
+						JOptionPane.showMessageDialog(null, "Welcome Admin! You can update and read file.");
+						break;
+					case Staff:
+						JOptionPane.showMessageDialog(null, "Welcome Staff! You can update, read, add, delete file.");
+						break;
+					case Student:
+						JOptionPane.showMessageDialog(null, "Welcome Student! You can only read file.");
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Good Bye!");
+					}
+				} else {
+					// JOptionPane.showMessageDialog(null, "Wrong password");
+
+				}
+			} else {
+				// System.out.println("Hello");
+			}
+			JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+			euser = JOptionPane.showInputDialog("Enter Username");
+		}
+
 	}
-
 }
